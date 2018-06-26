59d107bcadc2b47933abbaffe2f3e40af5304146
Lunad3 <123.daniel.luna@gmail.com>
2018-06-26 11:31:42 -0700

    made lots of changes to get indexto render on default used my db credentials, need to adjust

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 // Requiring our models for syncing
-// const db = require("./models");
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+const db = require("./models");
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static content for the app from the "public" directory in the application directory.app.use(express.static("public"));
+app.use(express.static("public"));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -24,10 +25,9 @@ app.use(bodyParser.json());
 // Import routes and give the server access to them. 
 // Routes
 // =============================================================
-require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
-// require("./routes/post-api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
-// db.sequelize.sync({ force: true }).then(function () {
-    app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
-// });
+db.sequelize.sync().then(function () {
+    app.listen(PORT, () => console.log("Listening on port" + PORT));
+});
