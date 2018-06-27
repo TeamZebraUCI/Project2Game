f67d405d20245a489271c77e50393b53d4ce9e49
Zachary Guns <zacharyguns@yahoo.com>
2018-06-26 22:30:01 -0700

    updated game logic: now the attack button stops working after the game has ended, so the player can no longer continue to click it and perform attacks

--- models/game.js
+++ models/game.js
@@ -30,87 +30,30 @@ $(document).ready(function () {
         if (dragonNum === playerNum) {
             // Subtract the damage amount from the dragon's health.
             dragonHealth -= playerDamage;
-            $("#combatText").text("You slash the dragon with " + playerDamage + " damage");
+            $("#combatText").text("Clean hit! You slashed the dragon for " + playerDamage + " damage");
         }
         else {
             // Subtract the damage amount from the player's health.
             playerHealth -= dragonDamage;
-            $("#combatText").text("OH NO! The dragon slashed you with " + dragonDamage + " damage");
+            $("#combatText").text("Damn! The dragon slashed you for " + dragonDamage + " damage");
 
         }
+
+        // update characters health after each round
         $("#playerHealthBar").text("Your health: " + playerHealth);
         $("#bossHealthBar").text("Boss health: " + dragonHealth);
+
+        if (playerHealth <= 0) {
+            // the boss wins
+            $("#combatText").text("You've been slain... Your story ends here...");
+            // prevent button from working after the game ends
+            $("#attackBtn").attr("onclick", "").unbind("click");
+        }
+        if (dragonHealth <= 0) {
+            // the player wins
+            $("#combatText").text("Victory! You've slain the dragon!");
+            // prevent button from working after the game ends
+            $("#attackBtn").attr("onclick", "").unbind("click");
+        }
     });
 });
-
-    // function checkRound() {
-
-    //     // If the player has less than 0 health.... then the player lost.
-    //     if (playerHealth <= 0) {
-    //         $("#combatText").text("You've been slain... Your story ends here...");
-    //        
-    //         // Exit the game
-    //         process.exit();
-    //     }
-
-    //     // If the dragon has less than 0 health.... then the player won.
-    //     if (dragonHealth <= 0) {
-    //         $("#combatText").text("Victory! You've slain the dragon!");
-    //          
-    //         // Exit the game
-    //         process.exit();
-    //     }
-
-    //     // After performing the "check", the next round is initiated.
-    //     playRound();
-
-    // }
-
-    // // =======================================================================================================
-    // // This function holds the game logic
-
-    // $("#attackBtn").prepend(function() {
-    //     let round = [{
-    //         type: "list",
-    //         name: "playerGuess",
-    //         message: "Try to stay alive! Guess a number between [1-3]",
-    //         choices: ["1", "2", "3",]
-    //     }]
-
-    //     // If the player is still alive or the dragon is still alive
-    //     if (playerHealth > 0 || dragonHealth > 0) {
-
-    //         // random number for the dragon's damage
-    //         let dragonDamage = Math.floor(Math.random() * 6) + 2;
-
-    //         // random number for the player's damage
-    //         let playerDamage = Math.floor(Math.random() * 5) + 1;
-
-    //         // The dragon should choose a random number.
-    //         let dragonNum = Math.floor((Math.random() * 3)) + 1;
-    //         $("#combatText").text("");
-    //         $("#combatText").text("");
-    //         $("#combatText").text("dragon rolled " + dragonNum);
-
-    //         // If the player's guess matches the number then...
-    //         if (dragonNum === parseInt(guess.playerGuess)) {
-
-    //             // Subtract the damage amount from the dragon's health.
-    //             dragonHealth -= playerDamage;
-    //             $("#combatText").text("You slash the dragon with " + playerDamage + " damage");
-    //         }
-
-    //         else {
-    //             // Subtract the damage amount from the player's health.
-    //             playerHealth -= dragonDamage;
-    //             $("#combatText").text("OH NO! The dragon slashed you with " + dragonDamage + " damage");
-
-    //         }
-    //         $("#combatText").text("You have " + playerHealth + " health left. The dragon has " + dragonHealth + " health left.");
-    //         // Check if the game is over.
-    //         checkRound();
-    //     }
-    // });
-
-    // // Starts the game!
-    // playRound()
