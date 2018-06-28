61707bfd9e4f47b84b64317ec73a6a029827641d
Lunad3 <123.daniel.luna@gmail.com>
2018-06-28 10:25:21 -0700

    moved js from createCharacter.html to /public/js folder and linked the creteCharacter.js at bottom of body

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
 
-  app.post("/api/signup", function(req, res) {
+  app.post("/api/signup", function(req, res){
     console.log(req.body);
     db.Users.create({
       userName: req.body.name,
